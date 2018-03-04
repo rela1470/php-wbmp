d274bd9ed35f1cf3dac0514ecefc6019980f2ea4
Jun Watanabe <j.watanabe@oratta.net>
2018-03-05 00:26:22 +0900

    fgetcで作り直し途中

--- viewer_max.php
+++ viewer_max.php
@@ -7,56 +7,35 @@
  */
 
 $br = (php_sapi_name() == 'cli') ? "\n" : "<br>";
+$fileName = './pic_127x127.wbmp';
+$fileName = './pic_8x8.wbmp';
 
-$data = file_get_contents('./pic_127x127.wbmp');
+$handle = fopen($fileName, 'r');
 
-$string = bin2hex($data);
+if (! $handle) {
+    echo $fileName . 'をオープンできませんでした';
+}
 
-$array = str_split($string, 2);
 
 //------------------------------
 //WBMP type
-$type = array_shift($array);
+$type = bindec(bin2bit(fgetc($handle)));
 if ( $type != 0) exit('WBMP type error');
+//------------------------------
 
 //------------------------------
 //Fixed header
-$type = array_shift($array);
-if ( $type != 0) exit('Fixed header error');
-
+$header = bindec(bin2bit(fgetc($handle)));
+if ( $header != 0) exit('Fixed header error');
 //------------------------------
-//size
-$width = hexdec(array_shift($array));
-$height = hexdec(array_shift($array));
 
-echo "width:{$width} height:{$height}{$br}";
-$output = [];
-foreach ((array)$array as $bits) {
-    $output[] = sprintf("%08s", decbin(hexdec($bits))). "{$br}";
-
+while (false !== ($bin = fgetc($handle))) {
+    echo sprintf("%08s",bin2bit($bin)). "{$br}";
 }
 
-$max = ($height > 8) ? 8 : $height;
-
-$count = 0;
-foreach ($output as $line) {
-    for ($i = 0; $i < $max; $i ++) {
-        $count ++;
-
-        //行がオクテットの途中で終わった
-        if ($count > $width) {
-            echo "{$br}";
-            $count = 0;
-            continue;//あとのビットはパディング分なので無視する
-        }
-
-        echo ($line[$i]) ? '□' : '■';
-    }
-
-    //行がオクテットの末尾でちょうど終わった
-    if ($count == $width) {
-        echo "{$br}";
-        $count = 0;
-        continue;//あとのビットはパディング分なので無視する
-    }
+function bin2bit($bin)
+{
+    $hex = bin2hex($bin);
+    $bit = base_convert($hex, 16, 2);
+    return sprintf("%08s",$bit);
 }
